4ae6dea93108e5b2012adb622368ee05e6b42706
rafibasha <rafibasha.sk@gmail.com>
2024-01-24 07:18:48 +0530

    Animation

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ import {
   Text,
   useColorScheme,
   View,
-  Button
+  Button,
 } from 'react-native';
 
 import {
@@ -25,21 +25,28 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
-import PracticeMemo from './PracticeMemo'
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import PracticeMemo from './PracticeMemo';
+import Home from './Home';
+import Example1 from './Example1';
 const Stack = createNativeStackNavigator();
 function DetailsScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Details Screen</Text>
-      
     </View>
   );
 }
-function HomeScreen({ navigation }) {
+function HomeScreen({navigation}) {
   return (
-    <View style={{ flex: 1, backgroundColor:'green',alignItems: 'center', justifyContent: 'center' }}>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'red',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
       <Text>Home Screen</Text>
       <Button
         title="Go to Details"
@@ -48,23 +55,21 @@ function HomeScreen({ navigation }) {
     </View>
   );
 }
-function App(){
-  
-
+function App() {
   return (
     <NavigationContainer>
-    <Stack.Navigator>
-    <Stack.Screen
-  name="Home"
-  component={HomeScreen}
-  options={{ title: 'Overview' }}
-/>
-      <Stack.Screen name="Details" component={PracticeMemo} />
-    </Stack.Navigator>
-  </NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{title: 'Overview'}}
+        />
+        <Stack.Screen name="Details" component={PracticeMemo} />
+        <Stack.Screen name="Example1" component={Example1} />
+        
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-
 
 export default App;
